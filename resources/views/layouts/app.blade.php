7df5a4024a3facc7fc4603e02e39bb6bfd598a87
SysAdmin <sysadmin@iMac-de-SysAdmin.local>
2020-01-14 15:11:56 -0600

    modificacion de pantalla de inicio de sesion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,92 +1,89 @@
 <!doctype html>
 <html lang="en">
-<head>
-	<meta charset="utf-8" />
-	<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-	<link rel="icon" type="image/png" href="../assets/img/favicon.png">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
-	<title>Sign Up Page - Material Kit by Creative Tim</title>
+        @yield('page-title') <!--Sección para cargar el título de la página-->
 
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+        <!--     Fonts and icons     -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
 
-	<!--     Fonts and icons     -->
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+        <!-- CSS Files -->
+        <!--Los asset buscan dentro la carpeta Public el contenido ubicado en su interior-->
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
+        <link href=" {{ asset('css/material-kit.css') }} " rel="stylesheet"/>
 
-    <!-- CSS Files -->
-    <!--Los asset buscan dentro la carpeta Public el contenido ubicado en su interior-->
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
-    <link href=" {{ asset('css/material-kit.css') }} " rel="stylesheet"/>
+    </head>
 
-</head>
+    <body class="@yield('body-class')">
+        <nav class="navbar navbar-transparent navbar-absolute">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="http://www.creative-tim.com">Creative Tim</a>
+                </div>
 
-<body>
-	<nav class="navbar navbar-transparent navbar-absolute">
-    	<div class="container">
-        	<!-- Brand and toggle get grouped for better mobile display -->
-        	<div class="navbar-header">
-        		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example">
-            		<span class="sr-only">Toggle navigation</span>
-		            <span class="icon-bar"></span>
-		            <span class="icon-bar"></span>
-		            <span class="icon-bar"></span>
-        		</button>
-        		<a class="navbar-brand" href="http://www.creative-tim.com">Creative Tim</a>
-        	</div>
+                <div class="collapse navbar-collapse" id="navigation-example">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="../components-documentation.html" target="_blank">
+                                Components
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://demos.creative-tim.com/material-kit-pro/presentation.html?ref=utp-freebie" target="_blank">
+                                <i class="material-icons">unarchive</i> Upgrade to PRO
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/CreativeTim" target="_blank" class="btn btn-simple btn-white btn-just-icon">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/CreativeTim" target="_blank" class="btn btn-simple btn-white btn-just-icon">
+                                <i class="fa fa-facebook-square"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/CreativeTimOfficial" target="_blank" class="btn btn-simple btn-white btn-just-icon">
+                                <i class="fa fa-instagram"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
-        	<div class="collapse navbar-collapse" id="navigation-example">
-        		<ul class="nav navbar-nav navbar-right">
-					<li>
-    					<a href="../components-documentation.html" target="_blank">
-    						Components
-    					</a>
-    				</li>
-    				<li>
-						<a href="http://demos.creative-tim.com/material-kit-pro/presentation.html?ref=utp-freebie" target="_blank">
-							<i class="material-icons">unarchive</i> Upgrade to PRO
-						</a>
-    				</li>
-		            <li>
-		                <a href="https://twitter.com/CreativeTim" target="_blank" class="btn btn-simple btn-white btn-just-icon">
-							<i class="fa fa-twitter"></i>
-						</a>
-		            </li>
-		            <li>
-		                <a href="https://www.facebook.com/CreativeTim" target="_blank" class="btn btn-simple btn-white btn-just-icon">
-							<i class="fa fa-facebook-square"></i>
-						</a>
-		            </li>
-					<li>
-		                <a href="https://www.instagram.com/CreativeTimOfficial" target="_blank" class="btn btn-simple btn-white btn-just-icon">
-							<i class="fa fa-instagram"></i>
-						</a>
-		            </li>
-        		</ul>
-        	</div>
-    	</div>
-    </nav>
+        <div class="wrapper">
+            @yield('content') <!--Se declara un contenido para que se incluyan los formularios (inicio sesión o registro)-->
+        </div>
 
-    <div class="wrapper">
+    </body>
 
-    </div>
+        <!--   Core JS Files   -->
+        <script src=" {{ asset('js/jquery.min.js') }} " type="text/javascript"></script>
+        <script src=" {{ asset('js/bootstrap.min.js') }} " type="text/javascript"></script>
+        <script src=" {{ asset('js/material.min.js') }} "></script>
 
-</body>
+        <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+        <script src=" {{ asset('js/nouislider.min.js') }} " type="text/javascript"></script>
 
-	<!--   Core JS Files   -->
-	<script src=" {{ asset('js/jquery.min.js') }} " type="text/javascript"></script>
-	<script src=" {{ asset('js/bootstrap.min.js') }} " type="text/javascript"></script>
-	<script src=" {{ asset('js/material.min.js') }} "></script>
+        <!--  Plugin for the Datepicker, full documentation here: http://www.eyecon.ro/bootstrap-datepicker/ -->
+        <script src=" {{ asset('js/bootstrap-datepicker.js') }} " type="text/javascript"></script>
 
-	<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-	<script src=" {{ asset('js/nouislider.min.js') }} " type="text/javascript"></script>
-
-	<!--  Plugin for the Datepicker, full documentation here: http://www.eyecon.ro/bootstrap-datepicker/ -->
-	<script src=" {{ asset('js/bootstrap-datepicker.js') }} " type="text/javascript"></script>
-
-	<!-- Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc -->
-	<script src=" {{ asset('js/material-kit.js') }} " type="text/javascript"></script>
+        <!-- Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc -->
+        <script src=" {{ asset('js/material-kit.js') }} " type="text/javascript"></script>
 
 </html>
 
