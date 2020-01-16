abd64f30f644b8e6387e205d5b7dbf58fdf5a310
SysAdmin <sysadmin@iMac-de-SysAdmin.local>
2020-01-16 15:27:44 -0600

    Logueo completado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,9 +13,12 @@
             <div class="row">
                 <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
                     <div class="card card-signup">
-                        <form class="form" method="" action="">
-                            <div class="header header-secondary text-center">
-                                <h4 style="background:black;">Inicio de sesión</h4>
+                        
+                        <form method="POST" action="{{ route('login') }}">
+                            {{ csrf_field() }} <!--token csrf tiene que estar presente en todas las peticiones POST-->
+                        
+                            <div class="header header-secondary text-center" style="background:black;">
+                                <h4>Inicio de sesión</h4>
                             </div>
 
                             <div class="content">
@@ -24,28 +27,31 @@
                                     <span class="input-group-addon">
                                         <i class="material-icons">email</i>
                                     </span>
-                                    <input type="text" class="form-control" placeholder="Email...">
+                                    <input id="email" type="email" class="form-control" placeholder="Email..." name="email" value="{{ old('email') }}" required autofocus>
                                 </div>
 
                                 <div class="input-group">
                                     <span class="input-group-addon">
                                         <i class="material-icons">lock_outline</i>
                                     </span>
-                                    <input type="password" placeholder="Password..." class="form-control" />
+                                    <input placeholder="Password..." id="password" type="password" class="form-control" name="password" required />
                                 </div>
 
-                                <!-- If you want to add a checkbox to this form, uncomment this code
+                                <!--If you want to add a checkbox to this form, uncomment this code-->
 
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="optionsCheckboxes" checked>
-                                        Subscribe to newsletter
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> <!--EL helper OLD ayuda a recuperar un valor anterior escrito en un input-->
+                                        Recordar sesión
                                     </label>
-                                </div> -->
+                                </div>
                             </div>
                             <div class="footer text-center">
-                                <a href="#pablo" class="btn btn-simple btn-primary btn-lg">Get Started</a>
+                                <button type="submit" class="btn btn-simple btn-primary btn-lg">Iniciar sesión</button>
                             </div>
+                            <!--<a class="btn btn-link" href="{{ route('password.request') }}">
+                                Forgot Your Password?
+                            </a>-->
                         </form>
                     </div>
                 </div>
@@ -64,70 +70,63 @@
     </div>
 @endsection
 
+
 <!--
-    <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+    <form method="POST" action="{{ route('login') }}">
+        {{ csrf_field() }} 
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+            <div class="col-md-6">
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+            <label for="password" class="col-md-4 control-label">Password</label>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+            <div class="col-md-6">
+                <input id="password" type="password" class="form-control" name="password" required>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+        <div class="form-group">
+            <div class="col-md-6 col-md-offset-4">
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                    </label>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+
+        <div class="form-group">
+            <div class="col-md-8 col-md-offset-4">
+                <button type="submit" class="btn btn-primary">
+                    Login
+                </button>
+
+                <a class="btn btn-link" href="{{ route('password.request') }}">
+                    Forgot Your Password?
+                </a>
+            </div>
+        </div>
+    </form>
+
 -->
+
+
+    
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,17 +30,36 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="">App Shop</a>
+                    <a class="navbar-brand" href="{{ url('/') }}">App Shop</a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="navigation-example">
                     <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="../components-documentation.html" target="_blank">
-                                Components
-                            </a>
-                        </li>
-                        <li>
+                        @guest
+                            <li><a href="{{ route('login') }}">Ingresar</a></li>
+                            <li><a href="{{ route('register') }}">Registro</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endguest
+                        <!--<li>
                             <a href="https://twitter.com/CreativeTim" target="_blank" class="btn btn-simple btn-white btn-just-icon">
                                 <i class="fa fa-twitter"></i>
                             </a>
@@ -54,7 +73,7 @@
                             <a href="https://www.instagram.com/CreativeTimOfficial" target="_blank" class="btn btn-simple btn-white btn-just-icon">
                                 <i class="fa fa-instagram"></i>
                             </a>
-                        </li>
+                        </li>-->
                     </ul>
                 </div>
             </div>
@@ -84,26 +103,26 @@
 
 <!--
 @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+    <li><a href="{{ route('login') }}">Login</a></li>
+    <li><a href="{{ route('register') }}">Register</a></li>
+@else
+    <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+            {{ Auth::user()->name }} <span class="caret"></span>
+        </a>
 
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
+        <ul class="dropdown-menu">
+            <li>
+                <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                    Logout
+                </a>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest  -->
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            </li>
+        </ul>
+    </li>
+@endguest  -->
